fc0defb4d64b75da041270020297cc1c59b18430
verruckterdrachen <verruckterdrachen@gmail.com>
2020-11-18 16:50:07 +0300

    add validation & map

--- src/js/main.js
+++ src/js/main.js
@@ -31,6 +31,68 @@ $(document).ready(function() {
 
     new WOW().init();
 
+    $('.modal__form').validate({
+        errorClass: "invalid",
+        rules: {
+            userName: {
+                required: true,
+                minlength: 2
+            },
+            userPhone: "required",
+            userEmail: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            userName: {
+                required: "Имя обязательно",
+                minlength: "Имя не короче двух букв"
+            },
+            userPhone: "Телефон обязателен",
+            userEmail: {
+                required: "Обязательно укажите e-mail",
+                email: "Введите в формате: name@domain.com"
+            }
+        }
+    });
+    $('.footer__form').validate({
+        errorClass: "invalid",
+        rules: {
+            userName: {
+                required: true,
+                minlength: 2
+            },
+            userPhone: "required",
+        },
+        messages: {
+            userName: {
+                required: "Имя обязательно",
+                minlength: "Имя не короче двух букв"
+            },
+            userPhone: "Телефон обязателен",
+        }
+    });
+    $('.control__form').validate({
+        errorClass: "invalid",
+        rules: {
+            userName: {
+                required: true,
+                minlength: 2
+            },
+            userPhone: "required",
+        },
+        messages: {
+            userName: {
+                required: "Имя обязательно",
+                minlength: "Имя не короче двух букв"
+            },
+            userPhone: "Телефон обязателен",
+        }
+    });
+
+    $('[type=tel]').mask('+7(000)-000-00-00', { placeholder: "+7(___)-___-__-__" });
+
 });
 
 /*
